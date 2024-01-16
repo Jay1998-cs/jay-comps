0566019e38a3e887d07bec84f446418d24c5b130
LiJie He <785537645@qq.com>
2024-01-16 23:13:10 +0800

    update

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 export interface BaseButtonProps {
   size?: "samll" | "media" | "large";
   msg?: string;
 }
 
-const Button: React.FC<BaseButtonProps> = (props) => {
-  const { msg = "" } = props;
+export interface ButtonProps extends BaseButtonProps {
+  href?: string;
+  htmlType?: "submit" | "button" | "reset";
+}
 
-  const btnClickHandler = () => {
-    console.log(msg + "【click button event】");
-  };
-
-  return (
-    <div onClick={btnClickHandler}>
-      <button style={{ color: "blue" }}>Button</button>
-    </div>
-  );
+const InternalButton: React.ForwardRefRenderFunction<
+  HTMLButtonElement | HTMLAnchorElement,
+  ButtonProps
+> = (props, ref) => {
+  const {} = props;
+  return <button>text</button>;
 };
+
+const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+  InternalButton
+);
 
 export default Button;
